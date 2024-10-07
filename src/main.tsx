61d5d2a213ada4e0d6c2f85ad96d81a0be197936
utelagazkodas <vargamiki53@gmail.com>
2024-10-07 19:08:06 +0200

    pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Index from './pages/Index.tsx'
 import './index.css'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+const router = createHashRouter([{
+  path: "/",
+  element: (<Index/>),
+  errorElement: (<div className='text-white'>404</div>)
+},{
+  path: "/dataProcessor",
+  element: (<div className='text-white'>data</div>)
+
+}])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    
+  </React.StrictMode>,
+) 
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 ) 
 
---
+++ src/pages/Index.tsx
@@ -0,0 +1,18 @@
+import { Background } from "../components/3dbackground"
+import { Content } from "../components/content"
+
+function Index(): JSX.Element {
+
+
+  return (
+    <>
+      
+        <Background/>
+
+        <Content/>
+      
+    </>
+  )
+}
+
+export default Index
