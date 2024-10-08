ce6e8068a46d60c595f50a8b932fa25acf3c8560
utelagazkodas <vargamiki53@gmail.com>
2024-10-08 17:48:53 +0200

    asd

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,6 +1,8 @@
+import { GooSpinner } from "react-spinners-kit"
+
 function Loading(): JSX.Element{
-    return(<div className="w-screen h-screen bg-black text-white text-center">
-        Loading...
+    return(<div className="w-screen h-screen bg-black text-white font-thin text-lg flex place-content-center items-center">
+        <GooSpinner size={70}/>
     </div>)
 }
 
